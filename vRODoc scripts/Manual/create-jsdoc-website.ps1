3cf0f520396ab2d71cfbdbbd8f98e3118aaf9d0e
Mayank Goyal <imtrinity94@gmail.com>
2022-02-02 19:13:25 +0530

    Update create-jsdoc-website.ps1

--- vRODoc scripts/Manual/create-jsdoc-website.ps1
+++ vRODoc scripts/Manual/create-jsdoc-website.ps1
@@ -1,1 +1,5 @@
-
+$savePath = $(Write-Host "Where are your Actions(.js)? (provide full path) -- " -NoNewLine -ForegroundColor yellow; Read-Host)
+#Set Slash
+$slash = "\"
+jsdoc --recurse $savePath$slash'Actions' -d $savePath$slash'docs'
+write-host "Actions(.js) converted to .html at  $savePath\docs";
